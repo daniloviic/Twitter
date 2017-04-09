d477f8cf7dd2b9eb15e79ef975461bbf6611929f
daniloviic <danilovic.milos96@gmail.com>
2017-04-09 19:00:13 +0200

    Uvedena nova grana za dokumentovanje. Izvrseno dokumentovanje klase TwitterPoruka kao i svih njenih atributa i metoda.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,30 +1,66 @@
 package com.twitter.poruke;
 
+/**
+ * 
+ * Klasa TwitterPoruka predstavlja twitter poruku.
+ * 
+ * @author Milos
+ * @version 1.0.0
+ */
 public class TwitterPoruka {
 
+	/**
+	 * Predstavlja korisnika twitter naloga.
+	 */
+
 	private String korisnik;
+	
+	/**
+	 * Predstavlja sadrzaj poruke.
+	 */
+
 	private String poruka;
 
+	/**
+	 * Ova metoda vraca korinika konkretnog twitter naloga.
+	 * @return korisnik
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
-
+	
+	/**
+	 * Ova metoda postavlja korisnika twitter naloga.
+	 * @param korisnik
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || !korisnik.isEmpty())
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
 
+	
+	/**
+	 * Ova metoda vraca sadrzaj twitter poruke.
+	 * @return poruka
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
-
+	
+	/**
+	 * Ova metoda postavlja sadrzaj twitter poruke.
+	 * @param poruka
+	 */
 	public void setPoruka(String poruka) {
 		if (this.poruka == null || this.poruka == new String("") || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
 
+	/**
+	 * Ova metoda ispisuje korisnika twitter naloga kao i sadrzaj poruke.
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
