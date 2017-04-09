9e386cbe88a82accbb976c19dcb9b3c3e2cc13cc
daniloviic <danilovic.milos96@gmail.com>
2017-04-09 21:13:53 +0200

    Izvrseno testiranje svih metoda klase Twitter.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -26,6 +26,10 @@ public class Twitter {
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
+	
+	public String vratiPoslednji(){
+		return poruke.getLast().getKorisnik()+ "+" + poruke.getLast().getPoruka();
+	}
 
 	
 	/**
@@ -36,7 +40,7 @@ public class Twitter {
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
-		tp.setKorisnik("korisnik");
+		tp.setKorisnik(korisnik);
 		tp.setPoruka(poruka);
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
@@ -69,9 +73,9 @@ public class Twitter {
 		// ona se upisuje u niz. Ako je prekoracen maxBroj,pretraga
 		// se prekida.
 		for (int i = 0; i < poruke.size(); i++)
-			if (poruke.get(i).getPoruka().indexOf(tag) != -1)
+			if (poruke.get(i).getPoruka().contains(tag))
 				if (brojac < maxBroj) {
-					rezultat[brojac + 1] = poruke.get(i);
+					rezultat[brojac] = poruke.get(i);
 					brojac++;
 				} else
 					break;
