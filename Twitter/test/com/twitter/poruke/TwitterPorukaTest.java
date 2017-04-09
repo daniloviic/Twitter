56e9127b5204bf6ee7cb77d3a85d3138220a6788
daniloviic <danilovic.milos96@gmail.com>
2017-04-09 19:16:35 +0200

    Uvedena grana za testiranje, napravljen novi source folder i novi Junit TestCase. Testovi jos uvek nisu napisani.

---
+++ Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.twitter.poruke;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Milos
+ *
+ */
+public class TwitterPorukaTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#setKorisnik(java.lang.String)}.
+	 */
+	@Test
+	public void testSetKorisnik() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#setPoruka(java.lang.String)}.
+	 */
+	@Test
+	public void testSetPoruka() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.twitter.poruke.TwitterPoruka#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
